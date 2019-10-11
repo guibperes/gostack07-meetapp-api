abbff234be95f958c85f6ecea2378431c2b6d436
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-10-11 14:20:20 -0300

    sequelize configured

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'guilherme',
+  password: '123456',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
