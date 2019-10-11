556e044a3efb88eb24ea6dc62fc07f0656c6d84a
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-10-11 14:31:47 -0300

    app class created

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from 'express'
+
+import routes from './routes'
+
+export class App {
+  constructor () {
+    this.server = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares () {
+    this.server.use(express.json())
+  }
+
+  routes () {
+    this.server.use(routes)
+  }
+
+  async start () {
+    try {
+      await this.server.listen(3000)
+
+      console.log('Server started on port 3000')
+    } catch (error) {
+      console.log(error)
+      process.exit(1)
+    }
+  }
+}
