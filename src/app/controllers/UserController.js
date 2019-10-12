07215161f1a5adea1611b5dd70468b45139d82f0
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-10-12 19:09:15 -0300

    user store controller created

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,33 @@
+import * as Yup from 'yup'
+
+import { User } from '../models/User'
+
+export class UserController {
+  async store (req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6)
+    })
+
+    try {
+      await schema.validate(req.body)
+    } catch (error) {
+      return res.status(400).json(error)
+    }
+
+    const { email: bodyEmail } = req.body
+
+    const userExists = await User.findOne({ where: { email: bodyEmail } })
+
+    if (userExists) {
+      return res.status(400).json({
+        message: 'User already exists'
+      })
+    }
+
+    const { id, name, email, provider } = await User.create(req.body)
+
+    return res.json({ id, name, email, provider })
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express'
 
+import { UserController } from './app/controllers/UserController'
+
 const routes = new Router()
 
-routes.get('/', (req, res) => res.json({ ok: true }))
+const userController = new UserController()
+
+routes.post('/users', userController.store)
 
 export default routes
