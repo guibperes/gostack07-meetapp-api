e007a8b72fc877b6f03398ca34a1df503cd678b6
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-10-13 09:35:48 -0300

    created update user method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -32,8 +32,49 @@ export class UserController {
   }
 
   async update (req, res) {
-    console.log(req.user)
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string()
+        .min(6)
+        .when('password', (password, field) =>
+          password ? field.required() : field
+        ),
+      password: Yup.string().min(6)
+    })
 
-    return res.json({ ok: true })
+    try {
+      await schema.validate(req.body)
+    } catch (error) {
+      return res.status(400).json(error)
+    }
+
+    const { email: bodyEmail, oldPassword } = req.body
+
+    const user = await User.findByPk(req.user)
+
+    if (bodyEmail !== user.email) {
+      const emailExists = User.findOne({ where: { email: bodyEmail } })
+
+      if (emailExists) {
+        return res.status(400).json({
+          message: 'Email is already in use'
+        })
+      }
+    }
+
+    if (oldPassword) {
+      const isValidPassword = await user.verifyPassword(oldPassword)
+
+      if (!isValidPassword) {
+        return res.status(400).json({
+          message: 'Password does not match'
+        })
+      }
+    }
+
+    const { id, name, email, provider } = await user.update(req.body)
+
+    return res.json({ id, name, email, provider })
   }
 }
