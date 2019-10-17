91108d0e22b0fdb15da9f6e72b42978e70263967
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-10-17 14:46:48 -0300

    added error handler and not found handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import 'express-async-errors'
 import morgan from 'morgan'
+import Youch from 'youch'
 
 import { IS_DEV, APP_PORT } from './config/env'
 import { Database } from './database'
@@ -16,10 +18,34 @@ class App {
   middlewares () {
     this.server.use(express.json())
     this.server.use(morgan(IS_DEV ? 'dev' : 'common'))
+    this.errorHandler()
+    this.notFoundHandler()
   }
 
   routes () {
     this.server.use(routes)
+  }
+
+  errorHandler () {
+    this.server.use((err, req, res, next) => {
+      if (IS_DEV) {
+        const errors = new Youch(err, req).toJSON()
+
+        return res.status(500).json(errors)
+      }
+
+      return res.status(500).json({
+        message: 'Internal Server Error'
+      })
+    })
+  }
+
+  notFoundHandler () {
+    this.server.use('*', (req, res) => {
+      return res
+        .status(404)
+        .json({ message: 'This route is not provided' })
+    })
   }
 
   async start () {
